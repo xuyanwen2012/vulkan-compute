0568e5003b61af6c1973434441bc26030843faa9
Yanwen Xu <xuyanwen2012@gmail.com>
2023-11-08 15:52:14 -0800

    s

--- src/core/command_buffer.hpp
+++ src/core/command_buffer.hpp
@@ -1,59 +1,58 @@
-// #pragma once
+#pragma once
 
-// #include "buffer.hpp"
+#include "buffer.hpp"
 
-// namespace core {
+namespace core {
 
 // struct BufferInfo {
 //   BufferReference resource;
 //   uint32_t offset = 0;
 // };
 
-// class CommandBuffer {
-// public:
-//   CommandBuffer(vk::CommandBuffer commandBuffer)
-//       : m_handle(std::move(commandBuffer)) {}
+class CommandBuffer {
+public:
+  CommandBuffer(vk::CommandBuffer commandBuffer)
+      : m_handle(std::move(commandBuffer)) {}
 
-//   // After begin, I should:
-//   // 1. Bind pipeline
-//   // 2. Bind descriptor set
-//   // 3. Push constants
-//   // 4. Dispatch
-//   // 5. End
-//   // 6. Submit (when I want)
-//   void Begin() {
-//     vk::CommandBufferBeginInfo commandBufferBeginInfo;
-//     commandBufferBeginInfo.setFlags(
-//         vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
-//     m_handle.begin(commandBufferBeginInfo);
-//   }
+  // After begin, I should:
+  // 1. Bind pipeline
+  // 2. Bind descriptor set
+  // 3. Push constants
+  // 4. Dispatch
+  // 5. End
+  // 6. Submit (when I want)
+  void Begin() {
+    constexpr auto begin_info = vk::CommandBufferBeginInfo().setFlags(
+        vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
+    m_handle.begin(begin_info);
+  }
 
-//   void Dispatch(uint32_t x, uint32_t y, uint32_t z);
+  void Dispatch(uint32_t x, uint32_t y, uint32_t z);
 
-//   // Bindings
-//   void BindDescriptorSet(vk::PipelineBindPoint bindPoint,
-//                          vk::PipelineLayout layout,
-//                          std::vector<vk::DescriptorSet> &descriptorSets);
+  // Bindings
+  void BindDescriptorSet(vk::PipelineBindPoint bindPoint,
+                         vk::PipelineLayout layout,
+                         std::vector<vk::DescriptorSet> &descriptorSets);
 
-//   //   void BindPipeline(PassNode *passNode);
+  // void BindPipeline(PassNode *passNode);
 
-//   //   // Commands (push const, special constants)
-//   //   void PushConstants(const PassNode *passNode, const uint8_t *data,
-//   //                      size_t size);
+  //   // Commands (push const, special constants)
+  //   void PushConstants(const PassNode *passNode, const uint8_t *data,
+  //                      size_t size);
 
-//   //   template <typename T>
-//   //   void PushConstant(const PassNode *node, std::span<T> constants) {
-//   //     PushConstants(node, (const uint8_t *)constants.data(),
-//   //                   constants.size() * sizeof(T));
-//   //   }
+  //   template <typename T>
+  //   void PushConstant(const PassNode *node, std::span<T> constants) {
+  //     PushConstants(node, (const uint8_t *)constants.data(),
+  //                   constants.size() * sizeof(T));
+  //   }
 
-//   //   template <typename T>
-//   //   void PushConstant(const PassNode *node, const T *constants) {
-//   //     PushConstants(node, (const uint8_t *)constants, sizeof(T));
-//   //   }
+  //   template <typename T>
+  //   void PushConstant(const PassNode *node, const T *constants) {
+  //     PushConstants(node, (const uint8_t *)constants, sizeof(T));
+  //   }
 
-// private:
-//   vk::CommandBuffer m_handle;
-// };
+private:
+  vk::CommandBuffer m_handle;
+};
 
-// } // namespace core
+} // namespace core
