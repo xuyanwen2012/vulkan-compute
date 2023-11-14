410d0c8cc57d4adcaeff7c0d678227dd6a668465
Yanwen Xu <xuyanwen2012@gmail.com>
2023-11-13 22:44:59 -0800

    s

--- xmake.lua
+++ xmake.lua
@@ -22,26 +22,33 @@ if is_mode("release") then
     set_optimize("fastest")
 end
 
-before_build(function (target) 
-    os.exec("./compile_shaders.sh")
-end)
+if is_plat("windows") then
+elseif is_plat("linux") then
+    before_build(
+        function(target)
+            os.exec("./compile_shaders.sh")
+        end
+    )
+end
 
-after_build(function (target) 
-    build_path = ""
-    if is_mode("release") then
-        build_path = "$(buildir)/linux/x86_64/release/"
-    else
-        build_path = "$(buildir)/linux/x86_64/debug/"
+after_build(
+    function(target)
+        platform = os.host()
+        build_path = ""
+        if is_mode("release") then
+            build_path = "$(buildir)/" .. platform .. "/x86_64/release/"
+        else
+            build_path = "$(buildir)/" .. platform .. "/x86_64/debug/"
+        end
+        os.cp("shaders/compiled_shaders/**.spv", build_path)
+        print("Copied compiled shaders to " .. build_path)
     end
-    os.cp("shaders/compiled_shaders/**.spv", build_path)
-    print("Copied compiled shaders to " .. build_path)
-end)
+)
 
 target("app")
     set_default(true)
     set_kind("binary")
     add_files("src/*.cpp", "src/core/*.cpp")
     add_headerfiles("src/core/*.hpp")
-    add_packages("vk-bootstrap", "vulkan-memory-allocator", "spirv-cross", "glm",
-                "vulkansdk")
+    add_packages("vk-bootstrap", "vulkan-memory-allocator", "spirv-cross", "glm", "vulkansdk")
     add_packages("spdlog", "cli11")
