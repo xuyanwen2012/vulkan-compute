fd31f454bf86080de38be7a6c13bfcd43da50d19
Yanwen Xu <xuyanwen2012@gmail.com>
2023-11-15 23:26:20 -0800

    script

--- compile_shaders.py
+++ compile_shaders.py
@@ -6,55 +6,80 @@ import subprocess
 input_folder = "shaders"
 output_folder = "shaders/compiled_shaders"
 
+
+def compile_with_clspv(cl_shader: str):
+    """
+    Compiles a given OpenCL shader to SPIR-V using clspv.
+
+    Args:
+        cl_shader (str): The filename of the OpenCL shader to compile.
+
+    Returns:
+        None
+    """
+    input_path = os.path.join(input_folder, cl_shader)
+    output_path = os.path.join(output_folder, os.path.splitext(cl_shader)[0] + ".spv")
+
+    clspv_command = [
+        "clspv",
+        "-w",
+        "-O0",
+        "--spv-version=1.3",
+        "--cl-std=CL2.0",
+        "-inline-entry-points",
+        input_path,
+        "-o",
+        output_path,
+    ]
+    print(" ".join(clspv_command))
+    subprocess.run(clspv_command)
+
+
+def compile_with_glslang(gl_shader: str):
+    """
+    Compiles a GLSL shader to SPIR-V using the glslangValidator tool.
+
+    Args:
+        gl_shader (str): The filename of the GLSL shader to compile.
+
+    Returns:
+        None
+    """
+    input_path = os.path.join(input_folder, gl_shader)
+    output_path = os.path.join(output_folder, os.path.splitext(gl_shader)[0] + ".spv")
+
+    glslang_command = [
+        "glslangValidator",
+        "-V",
+        "--target-env",
+        "spirv1.3",
+        input_path,
+        "-o",
+        output_path,
+    ]
+    print(" ".join(glslang_command))
+    subprocess.run(glslang_command)
+
+
 if __name__ == "__main__":
     os.makedirs(output_folder, exist_ok=True)
 
-    # Compile .cl shaders to .spv
-    cl_shaders = [
-        shader for shader in os.listdir(input_folder) if shader.endswith(".cl")
-    ]
-    for cl_shader in cl_shaders:
-        input_path = os.path.join(input_folder, cl_shader)
-        output_path = os.path.join(
-            output_folder, os.path.splitext(cl_shader)[0] + ".spv"
-        )
+    for shader in os.listdir(input_folder):
+        if shader.endswith(".comp"):
+            compile_with_glslang(shader)
+        elif shader.endswith(".cl"):
+            compile_with_clspv(shader)
+        else:
+            continue
 
-        clspv_command = [
-            "clspv",
-            "-w",
-            "-O0",
-            "--spv-version=1.3",
-            "--cl-std=CL2.0",
-            "-inline-entry-points",
-            input_path,
-            "-o",
-            output_path,
-        ]
-
-        print(" ".join(clspv_command))
-
-        subprocess.run(clspv_command)
-
-    # Compile .comp shaders to .spv
-    comp_shaders = [
-        shader for shader in os.listdir(input_folder) if shader.endswith(".comp")
-    ]
-    for comp_shader in comp_shaders:
-        input_path = os.path.join(input_folder, comp_shader)
-        output_path = os.path.join(
-            output_folder, os.path.splitext(comp_shader)[0] + ".spv"
-        )
-
-        glslang_command = [
-            "glslangValidator",
-            "-V",
-            "--target-env",
-            "spirv1.3",
-            input_path,
-            "-o",
-            output_path,
-        ]
-
-        subprocess.run(glslang_command)
+    # Copy the compiled shaders to the binary folder.
+    # if on linux, copy to the linux binary folder
+    if os.name == "posix":
+        os.system("cp shaders/compiled_shaders/* build/linux/x86_64/debug/")
+    elif os.name == "nt":
+        # os.system("copy shaders/compiled_shaders/* build/windows/x86_64/debug/")
+        pass
+    else:
+        print("OS not supported")
 
     print("Shaders compiled successfully.")
